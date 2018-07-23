a8157515f15b02ec47c0ec669d39bee4b43d95fe
anjie <18636657014@163.com>
2018-07-24 00:20:04 +0800

    update jap-bug-7.24

--- applyvue/src/pages/detail/count.vue
+++ applyvue/src/pages/detail/count.vue
@@ -1,18 +1,9 @@
 <template>
   <div class="sales-board">
     <div class="sales-board-intro">
-      <h2>基本信息</h2>
+      <h2>数据统计</h2>
       <p>历史资料、科学实验、检验、统计等所获得的和用于科学研究、技术设计、查证、决策等的数值加以统计为解决方案做前期准备。</p>
     </div>
-    <div class="sales-board-form">
-      <div class="sales-board-line">
-        <div class="sales-board-line-left">
-          个人照片：
-        </div>
-        <div class="sales-board-line-right">
-
-        </div>
-      </div>
       <div class="sales-board-line">
         <div class="sales-board-line-left">
           产品类型：
--- back/src/main/java/com/anjie/apply/controller/test.java
+++ back/src/main/java/com/anjie/apply/controller/test.java
@@ -1,14 +1,22 @@
 package com.anjie.apply.controller;
 
+import com.anjie.apply.domain.ProductOrder;
+import com.anjie.apply.repository.ProductOrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Iterator;
+import java.util.List;
+
 @RestController
 public class test {
-
+    @Autowired
+    ProductOrderRepository productOrderRepository;
     @GetMapping(value = "/test")
     public String girls() {
-        return "test-string";
+        long ss = productOrderRepository.count();
+        System.out.println(ss);
+        return "anjie";
     }
 }
