e6021683658d714259c1d9e85b6fc3d1e5789da6
anjie <18636657014@163.com>
2018-08-07 23:46:57 +0800

    update spring security

---
+++ back/src/main/java/com/anjie/apply/domain/SysUser.java
@@ -0,0 +1,144 @@
+package com.anjie.apply.domain;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+public class SysUser implements UserDetails{//实现此接口，注册SysUser为spring Security说使用
+    //private static final Long serialVersionUID=1L;
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+    private String password;
+    private String nickname;
+    private String email;
+    private String enabled;
+    private String phone;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(String enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getUserface() {
+        return userface;
+    }
+
+    public void setUserface(String userface) {
+        this.userface = userface;
+    }
+
+    public String getRegtime() {
+        return regtime;
+    }
+
+    public void setRegtime(String regtime) {
+        this.regtime = regtime;
+    }
+
+    public List<SysRole> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<SysRole> roles) {
+        this.roles = roles;
+    }
+
+
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    private String userface;
+    private String regtime;
+
+    @ManyToMany(cascade = {CascadeType.REFRESH},fetch = FetchType.EAGER)//配置用户和角色的多对多关系
+    private List<SysRole> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {//重写此方法将用户的角色作为权限
+        List<GrantedAuthority> auths=new ArrayList<>();
+        this.getRoles();
+        for (SysRole sysrole:roles
+             ) {
+            auths.add(new SimpleGrantedAuthority(sysrole.getRolename()));
+        }
+        return auths;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
